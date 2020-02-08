7aa988f6dcd3e281ee0cb858a52fb4a34e44f9aa
forest1030 <surim.sohn@gmail.com>
2020-02-08 18:55:19 +0900

    css 추가

--- name.js
+++ name.js
@@ -1,3 +1,16 @@
+const form = document.querySelector(".js-form"),
+    input = document.querySelector("input");
+
+const user_local = "currentUser"
+
+function loadName(){
+    const currentUser = localStorage.getItem(user_local);
+    if (currentUser === null){  //user가 없을 때 하는 행동
+
+    }else{  //user가 있을때 하는 행동
+
+    }
+}
 function init(){}
 
 init();
